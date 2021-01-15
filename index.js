66629912f22d93e4a8733fb95cd84935ba3945b7
mohammed sikder <sikderismail@gmail.com>
2021-01-15 00:20:15 -0500

    added to addess-book collection

--- index.js
+++ index.js
@@ -42,3 +42,28 @@ mongoose.connect(collectionsStr,
         .then(address=>res.send(address))
         .catch(err=>console.log(err))
     })
+
+    //retrieve the user from database byb id
+    app.get('/user/:id', (req,res)=>{
+
+        Address.findById(req.params.id)
+        .then(address=>res.send(address), console.log('found'))
+        .catch(err=>console.log('address not there'))
+        // .then(()=>console.log('address found'))
+        // .catch(err=>console.log('address not there'))
+    })
+
+    //update a user address by id
+    app.put('/update/:id',(req,res)=>{
+        let address ={}
+        if(req.body.name) address.name=req.body.name
+        if(req.body.email) address.email=req.body.email
+        if(req.body.phoneNumber) address.phoneNumber=req.body.phoneNumber
+        if(req.body.place) address.place = req.body.place
+
+        address = {$set:address}
+
+        Address.update({_id:req.params.id},address)
+        .then(()=>res.send(address))
+        .catch(err=>console.log(err))
+    })
